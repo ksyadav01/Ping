3363ac75eda58b500f2a98b668e837e60f533c65
Nikhil Bodduluri <nboss0527@gmail.com>
2021-06-23 17:27:29 -0400

    karans duty

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -6,8 +6,11 @@ import * as Location from 'expo-location';
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
 
   useEffect(() => {
+    
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -17,19 +20,42 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+       setLat(location["coords"]["latitude"])
+       setLong(location["coords"]["longitude"])
     })();
+   
   }, []);
 
   let text = 'Waiting..';
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
+
     text = JSON.stringify(location);
+    
+    
   }
 
+  
+
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
+      {/* <Text>{lat}</Text>
+      <Text>{long}</Text> */}
+      
+      
+      
+      <MapView style={styles.map} 
+      
+      initialRegion={{
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }} />
+   
     </View>
   );
 }
@@ -37,9 +63,14 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    }
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      },
 
 
 })
